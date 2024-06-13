041136942cfa0870674ba5a474f0d56fc5507a7e
AnnsAnn <git@annsann.eu>
2024-06-13 19:53:56 +0200

    Minecraft 1.21 Support

---
+++ fabric/remappedSrc/eu/annsann/flowerpower/Flowers.java
@@ -0,0 +1,33 @@
+package eu.annsann.flowerpower;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.FlowerbedBlock;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.tag.TagKey;
+import net.minecraft.util.Identifier;
+
+public class Flowers {
+    public static final FlowerbedBlock RED_PETALS = GenericPetalHelper.createNewPetal();
+    public static final FlowerbedBlock YELLOW_PETALS = GenericPetalHelper.createNewPetal();
+    public static final FlowerbedBlock BLUE_PETALS = GenericPetalHelper.createNewPetal();
+    public static final FlowerbedBlock ORCHID_PETALS = GenericPetalHelper.createNewPetal();
+    public static final FlowerbedBlock ORANGE_PETALS = GenericPetalHelper.createNewPetal();
+    public static final FlowerbedBlock GREY_PETALS = GenericPetalHelper.createNewPetal();
+    public static final FlowerbedBlock PINK_PETALS = GenericPetalHelper.createNewPetal();
+    public static final FlowerbedBlock WHITE_PETALS = GenericPetalHelper.createNewPetal();
+    public static final FlowerbedBlock MAGENTA_PETALS = GenericPetalHelper.createNewPetal();
+    public static final FlowerbedBlock BLACK_PETALS = GenericPetalHelper.createNewPetal();
+
+    public static final Block[] Petals = {
+            RED_PETALS,
+            YELLOW_PETALS,
+            BLUE_PETALS,
+            ORCHID_PETALS,
+            ORANGE_PETALS,
+            GREY_PETALS,
+            PINK_PETALS,
+            WHITE_PETALS,
+            MAGENTA_PETALS,
+            BLACK_PETALS
+    };
+}
